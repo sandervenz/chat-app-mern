f2915a04186069891ff6e15180b491b20477b329
Sander <sandervdb23@gmail.com>
2025-01-10 22:18:59 +0700

    update cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://yapper-ten.vercel.app/",
+    origin: "https://yapper-ten.vercel.app",
     credentials: true,
   })
 );
