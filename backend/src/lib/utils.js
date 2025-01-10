b1ba6ec8ba83a1592b4085c8451af2db7cdbbfea
Sander <sandervdb23@gmail.com>
2025-01-10 23:58:52 +0700

    done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,12 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari
+    httpOnly: true,                 // Cegah akses JS
+    secure: process.env.NODE_ENV === "production", // Hanya HTTPS di production
+    sameSite: "strict",             // Cegah CSRF
   });
+  
 
   return token;
 };
