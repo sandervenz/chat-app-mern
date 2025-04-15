ce2f325a7e0d4859d1ab5743522d2e0fba495cac
Sander <sandervdb23@gmail.com>
2025-04-15 21:35:59 +0700

    Update BASE_URL in axios.js and useAuthStore.js for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5001/api'
-    : 'https://chat-app-mern.railway.internal/api';
+    : 'https://chat-app-mern-production-f89a.up.railway.app/api';
 
 export const axiosInstance = axios.create({
   baseURL,
