c8580dcfe051b1571b676d731926257cfadd3371
Sander <sandervdb23@gmail.com>
2025-04-15 17:13:29 +0700

    update axios baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5001/api'
-    : 'chat-app-mern-tawny.vercel.app/api';
+    : 'https://chat-app-mern-tawny.vercel.app/api';
 
 export const axiosInstance = axios.create({
   baseURL,
