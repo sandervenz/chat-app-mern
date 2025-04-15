b97e4a21a227918f3c9fff598948bacbf2b6660a
Sander <sandervdb23@gmail.com>
2025-04-15 17:09:06 +0700

    update CORS configuration to allow localhost for development

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://yapper-ten.vercel.app",
+    origin: ["https://yapper-ten.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,12 @@
+// lib/axios.js
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5001/api'
+    : 'chat-app-mern-tawny.vercel.app/api';
+
 export const axiosInstance = axios.create({
-    baseURL: 'https://chat-app-mern-tawny.vercel.app/api',
-    withCredentials: true,
+  baseURL,
+  withCredentials: true,
 });
