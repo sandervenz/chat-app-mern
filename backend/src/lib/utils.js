0e5347637c3fc02d14defdd2d5ec1be91fa22f47
Sander <sandervdb23@gmail.com>
2025-04-15 17:57:17 +0700

    Update backend/utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,11 +6,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari
-    httpOnly: true,                 // Cegah akses JS
-    secure: process.env.NODE_ENV === "production", // Hanya HTTPS di production
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
   });
-  
 
   return token;
 };
